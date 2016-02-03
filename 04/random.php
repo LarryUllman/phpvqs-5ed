ead0ade74e599db8bb4c297bd524177cf855c640
Larry Ullman <Larry@LarryUllman.com>
2016-02-02 20:13:36 -0500

    add code for ch 4

---
+++ 04/random.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Lucky Numbers</title>
+</head>
+<body>
+<?php // Script 4.6 - random.php
+/* This script generates 3 random numbers. */
+
+// Address error handling, if you want.
+
+// Create three random numbers:
+$n1 = rand(1, 99);
+$n2 = rand(1, 99);
+$n3 = rand(1, 99);
+
+// Print out the numbers:
+print "<p>Your lucky numbers are:<br>
+$n1<br>
+$n2<br>
+$n3</p>";
+
+?>
+</body>
+</html>
